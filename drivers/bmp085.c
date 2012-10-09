00e0909bbb6ec7180aee5a48404880e514e2c2fa
sebseb7 <github@seb.exse.net>
2012-10-09 13:38:30 +0200

    bmp085 driver completed

--- drivers/bmp085.c
+++ drivers/bmp085.c
@@ -10,6 +10,30 @@ extern volatile uint32_t I2CMasterState;
 extern volatile uint32_t I2CReadLength, I2CWriteLength;
 
 
+#define CAL_AC1           0xAA  // R   Calibration data (16 bits)
+#define CAL_AC2           0xAC  // R   Calibration data (16 bits)
+#define CAL_AC3           0xAE  // R   Calibration data (16 bits)    
+#define CAL_AC4           0xB0  // R   Calibration data (16 bits)
+#define CAL_AC5           0xB2  // R   Calibration data (16 bits)
+#define CAL_AC6           0xB4  // R   Calibration data (16 bits)
+#define CAL_B1            0xB6  // R   Calibration data (16 bits)
+#define CAL_B2            0xB8  // R   Calibration data (16 bits)
+#define CAL_MB            0xBA  // R   Calibration data (16 bits)
+#define CAL_MC            0xBC  // R   Calibration data (16 bits)
+#define CAL_MD            0xBE  // R   Calibration data (16 bits)
+#define CONTROL_OUTPUT    0xF6  // R   Output registers 0xF6=MSB, 0xF7=LSB, 0xF8=XLSB
+#define CONTROL           0xF4  // W   Control register 
+#define READ_TEMPERATURE        0x2E 
+#define READ_PRESSURE           0x34 
+#define MODE_ULTRA_LOW_POWER    0 //oversampling=0, internalsamples=1, maxconvtimepressure=4.5ms, avgcurrent=3uA, RMSnoise_hPA=0.06, RMSnoise_m=0.5
+#define MODE_STANDARD           1 //oversampling=1, internalsamples=2, maxconvtimepressure=7.5ms, avgcurrent=5uA, RMSnoise_hPA=0.05, RMSnoise_m=0.4
+#define MODE_HIGHRES            2 //oversampling=2, internalsamples=4, maxconvtimepressure=13.5ms, avgcurrent=7uA, RMSnoise_hPA=0.04, RMSnoise_m=0.3
+#define MODE_ULTRA_HIGHRES      3 //oversampling=3, internalsamples=8, maxconvtimepressure=25.5ms, avgcurrent=12uA, RMSnoise_hPA=0.03, RMSnoise_m=0.25
+
+int16_t ac1,ac2,ac3,b1,b2,mb,mc,md;               // cal data  
+uint16_t ac4,ac5,ac6;                     // cal data
+int32_t b5;                                      // temperature data
+uint32_t pressure;
 
 
 static void i2cWrite(uint8_t reg, uint8_t value)
@@ -87,7 +111,7 @@ void BMP085_getCalData(void) {
 
 void BMP085_readTemperature(void){
 
-	long ut,x1,x2;
+	int32_t ut,x1a,x2a;
 
 	//read Raw Temperature
 	i2cWrite(CONTROL, READ_TEMPERATURE);
@@ -97,30 +121,55 @@ void BMP085_readTemperature(void){
 //	draw_number_8x6(60,00, ut, 6,0,255,255,0);
 
 	// calculate temperature
-	x1 = ((long)ut - ac6) * ac5 >> 15;
-	x2 = ((long)mc << 11) / (x1 + md);
-	b5 = x1 + x2;
+	x1a = ((int32_t)ut - ac6) * ac5 >> 15;
+	x2a = ((int32_t)mc << 11) / (x1a + md);
+	b5 = x1a + x2a;
 }
 
+uint16_t BMP085_getTemperatrue(void)
+{
+	return ((b5 + 8) >> 4);
+}
 
-void BMP085_readPressure(void) {
-	long up,x1,x2,x3,b3,b6,p;
-	unsigned long b4,b7;
-	int32_t tmp; 
+void BMP085_readPressure(void)
+{
+	int32_t up=0,x1,x2,x3,b3,b6,p;
+	uint32_t b4,b7;
 
 	//read Raw Pressure
 	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
 	delay_ms(26);
-	up = i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+/*	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
+	delay_ms(26);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
+	delay_ms(26);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
+	delay_ms(26);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
+	delay_ms(26);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
+	delay_ms(26);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
+	delay_ms(26);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
+	i2cWrite(CONTROL, READ_PRESSURE+(MODE_ULTRA_HIGHRES << 6));
+	delay_ms(26);
+	up += i2cReads24(0xF6) >> (8-MODE_ULTRA_HIGHRES);
 	
+	up = up >> 3;
+*/
 	// calculate true pressure
-	b6 = b5 - 4000;             // b5 is updated by calcTrueTemperature().
+	b6 = b5 - 4000;             // b5 is updated by BMP085_readTemperature().
 	x1 = (b2* (b6 * b6 >> 12)) >> 11;
 	x2 = ac2 * b6 >> 11;
 	x3 = x1 + x2;
-	tmp = ac1;
-	tmp = (tmp * 4 + x3) << MODE_ULTRA_HIGHRES;
-	b3 = (tmp + 2) >> 2;
+	b3 = (((ac1*4+x3) << MODE_ULTRA_HIGHRES)+2) >> 2;
 	x1 = ac3 * b6 >> 13;
 	x2 = (b1 * (b6 * b6 >> 12)) >> 16;
 	x3 = ((x1 + x2) + 2) >> 2;
@@ -130,31 +179,18 @@ void BMP085_readPressure(void) {
 	x1 = (p >> 8) * (p >> 8);
 	x1 = (x1 * 3038) >> 16;
 	x2 = (-7357 * p) >> 16;
-	uint32_t pressure = p + ((x1 + x2 + 3791) >> 4);
-
-
-	int32_t centimeters = 0;
-	centimeters = 4433000 * (1 - pow((pressure / (float)99612), 0.1903f));  
-//	lcdFillRGB(0,0,0);
-//	draw_number_8x6(60,80, pressure, 6,0,255,255,0);
-//	draw_number_8x6(60,90, centimeters, 6,0,255,0,255);
-
-/*
-	int centimeters
-	samples[index++] = centimeters;
-	index %= 10;
-	int average = 0;
-	for (int i = 0; i < 10; i++) {
-		average += samples[i];
-	}
-	average /= 10;
-	draw_number_8x6(60,100, average, 6,0,255,0,255);
-*/
+	pressure = p + ((x1 + x2 + 3791) >> 4);
+}
+uint32_t BMP085_getPressure(void)
+{
+	return pressure;
+}
+uint32_t BMP085_getAltitude(uint32_t pressureAtSeaLevel)
+{
+	return 4433000 * (1 - pow((pressure / (float)pressureAtSeaLevel), 0.1903f));  
 }
 
-/*
-		BMP085_readTemperature();
-		draw_number_8x6(60,10, temperature, 6,0,0,0,0);
-		temperature = ((b5 + 8) >> 4);
-		draw_number_8x6(60,10, temperature, 6,0,255,255,255);
-*/
+uint32_t BMP085_getPressureAtSeaLevel(uint32_t altitude)
+{
+	return pressure / (float)pow( 1-(altitude/ 4433000.0f), 5.255f);  
+}
--- drivers/bmp085.h
+++ drivers/bmp085.h
@@ -3,32 +3,14 @@
 
 #include "main.h"
 
-#define CAL_AC1           0xAA  // R   Calibration data (16 bits)
-#define CAL_AC2           0xAC  // R   Calibration data (16 bits)
-#define CAL_AC3           0xAE  // R   Calibration data (16 bits)    
-#define CAL_AC4           0xB0  // R   Calibration data (16 bits)
-#define CAL_AC5           0xB2  // R   Calibration data (16 bits)
-#define CAL_AC6           0xB4  // R   Calibration data (16 bits)
-#define CAL_B1            0xB6  // R   Calibration data (16 bits)
-#define CAL_B2            0xB8  // R   Calibration data (16 bits)
-#define CAL_MB            0xBA  // R   Calibration data (16 bits)
-#define CAL_MC            0xBC  // R   Calibration data (16 bits)
-#define CAL_MD            0xBE  // R   Calibration data (16 bits)
-#define CONTROL_OUTPUT    0xF6  // R   Output registers 0xF6=MSB, 0xF7=LSB, 0xF8=XLSB
-#define CONTROL           0xF4  // W   Control register 
-#define READ_TEMPERATURE        0x2E 
-#define READ_PRESSURE           0x34 
-#define MODE_ULTRA_LOW_POWER    0 //oversampling=0, internalsamples=1, maxconvtimepressure=4.5ms, avgcurrent=3uA, RMSnoise_hPA=0.06, RMSnoise_m=0.5
-#define MODE_STANDARD           1 //oversampling=1, internalsamples=2, maxconvtimepressure=7.5ms, avgcurrent=5uA, RMSnoise_hPA=0.05, RMSnoise_m=0.4
-#define MODE_HIGHRES            2 //oversampling=2, internalsamples=4, maxconvtimepressure=13.5ms, avgcurrent=7uA, RMSnoise_hPA=0.04, RMSnoise_m=0.3
-#define MODE_ULTRA_HIGHRES      3 //oversampling=3, internalsamples=8, maxconvtimepressure=25.5ms, avgcurrent=12uA, RMSnoise_hPA=0.03, RMSnoise_m=0.25
-int16_t ac1,ac2,ac3,b1,b2,mb,mc,md;               // cal data  
-uint16_t ac4,ac5,ac6;                     // cal data
-long b5;                                      // temperature data
+void BMP085_getCalData(void); 								//has to be called once for initialization
 
-void BMP085_readPressure(void);
 void BMP085_readTemperature(void);
-void BMP085_getCalData(void);
+void BMP085_readPressure(void);
+uint16_t BMP085_getTemperatrue(void);						//BMP085_readTemperature has to be called before this
+uint32_t BMP085_getPressure(void);							//BMP085_readTemperature/readPressure has to be called before this
+uint32_t BMP085_getAltitude(uint32_t pressureAtSeaLevel); 	// mean: 101325  & BMP085_readTemperature/readPressure has to be called before this
+uint32_t BMP085_getPressureAtSeaLevel(uint32_t altitude); 	//BMP085_readTemperature/readPressure has to be called before this
 
 
 
