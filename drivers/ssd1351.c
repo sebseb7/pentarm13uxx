6df36203b77b3d99a418f9b43c87efaeecdf2388
sebseb7 <github@seb.exse.net>
2012-11-03 05:56:44 +0100

    correct 6800 mode

--- drivers/ssd1351.c
+++ drivers/ssd1351.c
@@ -1,5 +1,4 @@
 #include "ssd1351.h"
-//#include "ssp.h"
 
 static lcdProperties_t ssd1351Properties = { 128, 128 };
 
@@ -7,51 +6,111 @@ static lcdProperties_t ssd1351Properties = { 128, 128 };
 /* Private Methods                               */
 /*************************************************/
 
-//#define CMD(c)        do {  CLR_DC; ssd1351SendByte( c );  } while (0)
 #define CMD(c)        do {   CLR_CS;CLR_DC; ssd1351SendByte( c );SET_CS;  } while (0)
 #define DATA(c)       do {  CLR_CS;SET_DC; ssd1351SendByte( c ); SET_CS; } while (0);
-//#define DATA(c)       do {  SET_DC; ssd1351SendByte( c );  } while (0);
-#define DELAY(mS)     do { delay_ms(mS); } while(0);
 
 void ssd1351SendByte(uint8_t byte)
 {
 
 #ifndef I6800
- 
-  int8_t i;
 
-  // Write from MSB to LSB
-  for (i=7; i>=0; i--) 
-  {
-    // Set clock pin low
-    CLR_SCK;
-    // Set data pin high or low depending on the value of the current bit
-    if (byte & (1 << i))
-    {
-      SET_SID;
-    }
-    else
-    {
-      CLR_SID;
-    }
-    // Set clock pin high
-    SET_SCK;
-  }
+	int8_t i;
+
+	// Write from MSB to LSB
+	__ASM volatile ("nop");
+	for (i=7; i>=0; i--) 
+	{
+		// Set clock pin low
+		__ASM volatile ("nop");
+		CLR_SCK;
+		__ASM volatile ("nop");
+		// Set data pin high or low depending on the value of the current bit
+		if (byte & (1 << i))
+		{
+			SET_SID;
+		}
+		else
+		{
+			CLR_SID;
+		}
+		// Set clock pin high
+		__ASM volatile ("nop");
+		SET_SCK;
+	}
+	__ASM volatile ("nop");
 #endif
 
 #ifdef I6800
 
 
-
 	SET_E;
 
-	LPC_GPIO0->MASKED_ACCESS[((1<<6)|(1<<7))]=byte<<6;
-	LPC_GPIO1->MASKED_ACCESS[((1<<3)|(1<<4)|(1<<5))]=byte;
-	LPC_GPIO2->MASKED_ACCESS[(1<<3)]=byte<<1;
-	LPC_GPIO3->MASKED_ACCESS[(1<<3)]=byte>>3;
-	LPC_GPIO2->MASKED_ACCESS[(1<<6)]=byte>>1;
+	if (byte & (1 << 0))
+	{
+		SET_SCK;
+	}
+	else
+	{
+		CLR_SCK;
+	}
+	if (byte & (1 << 1))
+	{
+		SET_SID;
+	}
+	else
+	{
+		CLR_SID;
+	}
+	if (byte & (1 << 2))
+	{
+		SET_D2;
+	}
+	else
+	{
+		CLR_D2;
+	}
+	if (byte & (1 << 3))
+	{
+		SET_D3;
+	}
+	else
+	{
+		CLR_D3;
+	}
+	if (byte & (1 << 4))
+	{
+		SET_D4;
+	}
+	else
+	{
+		CLR_D4;
+	}
+	if (byte & (1 << 5))
+	{
+		SET_D5;
+	}
+	else
+	{
+		CLR_D5;
+	}
+	if (byte & (1 << 6))
+	{
+		SET_D6;
+	}
+	else
+	{
+		CLR_D6;
+	}
+	if (byte & (1 << 7))
+	{
+		SET_D7;
+	}
+	else
+	{
+		CLR_D7;
+	}
 
-  __ASM volatile ("nop");
+	__ASM volatile ("nop");
 
 	CLR_E;
 
@@ -99,11 +158,11 @@ void lcdInit(void)
 	//Reset the LCD
 
 	SET_RST;
-	DELAY(20);
+	Delay(20);
 	CLR_RST;
-	DELAY(200);
+	Delay(200);
 	SET_RST;
-	DELAY(20);
+	Delay(20);
 
 	CMD(SSD1351_CMD_SETCOMMANDLOCK);
 	DATA(0x12);                               // Unlocked to enter commands
@@ -120,7 +179,7 @@ void lcdInit(void)
 	DATA(0xB4);
 #endif
 #ifndef C262K
-//#warning 65k colors
+	//#warning 65k colors
 	DATA(0x74);                               // Bit 7:6 = 65,536 Colors, Bit 3 = BGR or RGB
 #endif
 	CMD(SSD1351_CMD_SETCOLUMNADDRESS);
@@ -164,142 +223,142 @@ void lcdInit(void)
 
 	// Use default grayscale for now to save flash space (1k), but here are
 	// the values if someone wants to change them ...
-/*	CMD(SSD1351_CMD_GRAYSCALELOOKUP);
+	/*	CMD(SSD1351_CMD_GRAYSCALELOOKUP);
 
-DATA(0x02);	 // Gray Scale Level 1
-DATA(0x03);	 // Gray Scale Level 2
-DATA(0x04);	 // Gray Scale Level 3
-DATA(0x05);	 // Gray Scale Level 4
+		DATA(0x02);	 // Gray Scale Level 1
+		DATA(0x03);	 // Gray Scale Level 2
+		DATA(0x04);	 // Gray Scale Level 3
+		DATA(0x05);	 // Gray Scale Level 4
 
-DATA(0x06);	 // Gray Scale Level 5
-DATA(0x07);	 // Gray Scale Level 6
-DATA(0x08);	 // Gray Scale Level 7
-DATA(0x09);	 // Gray Scale Level 8
+		DATA(0x06);	 // Gray Scale Level 5
+		DATA(0x07);	 // Gray Scale Level 6
+		DATA(0x08);	 // Gray Scale Level 7
+		DATA(0x09);	 // Gray Scale Level 8
 
-DATA(0x0A);	 // Gray Scale Level 9
-DATA(0x0B);	 // Gray Scale Level 10
-DATA(0x0C);	 // Gray Scale Level 11
-DATA(0x0D);	 // Gray Scale Level 12
+		DATA(0x0A);	 // Gray Scale Level 9
+		DATA(0x0B);	 // Gray Scale Level 10
+		DATA(0x0C);	 // Gray Scale Level 11
+		DATA(0x0D);	 // Gray Scale Level 12
 
-DATA(0x0E);	 // Gray Scale Level 13
-DATA(0x0F);	 // Gray Scale Level 14
+		DATA(0x0E);	 // Gray Scale Level 13
+		DATA(0x0F);	 // Gray Scale Level 14
 
-DATA(0x10);	 // Gray Scale Level 15
-DATA(0x11);	 // Gray Scale Level 16
-DATA(0x12);	 // Gray Scale Level 17
-DATA(0x13);	 // Gray Scale Level 18
-DATA(0x15);	 // Gray Scale Level 19
-DATA(0x17);	 // Gray Scale Level 20
-DATA(0x19);	 // Gray Scale Level 21
-DATA(0x1B);	 // Gray Scale Level 22
-DATA(0x1D);	 // Gray Scale Level 23
-DATA(0x1F);	 // Gray Scale Level 24
-DATA(0x21);	 // Gray Scale Level 25
-DATA(0x23);	 // Gray Scale Level 26
-DATA(0x25);	 // Gray Scale Level 27
-DATA(0x27);	 // Gray Scale Level 28
-DATA(0x2A);	 // Gray Scale Level 29
-DATA(0x2D);	 // Gray Scale Level 30
-DATA(0x30);	 // Gray Scale Level 31
-DATA(0x33);	 // Gray Scale Level 32
-DATA(0x36);	 // Gray Scale Level 33
-DATA(0x39);	 // Gray Scale Level 34
-DATA(0x3C);	 // Gray Scale Level 35
-DATA(0x3F);	 // Gray Scale Level 36
-DATA(0x42);	 // Gray Scale Level 37
-DATA(0x45);	 // Gray Scale Level 38
-DATA(0x48);	 // Gray Scale Level 39
-DATA(0x4C);	 // Gray Scale Level 40
-DATA(0x50);	 // Gray Scale Level 41
-DATA(0x54);	 // Gray Scale Level 42
-DATA(0x58);	 // Gray Scale Level 43
-DATA(0x5C);	 // Gray Scale Level 44
-DATA(0x60);	 // Gray Scale Level 45
-DATA(0x64);	 // Gray Scale Level 46
-DATA(0x68);	 // Gray Scale Level 47
-DATA(0x6C);	 // Gray Scale Level 48
-DATA(0x70);	 // Gray Scale Level 49
-DATA(0x74);	 // Gray Scale Level 50
-DATA(0x78);	 // Gray Scale Level 51
-DATA(0x7D);	 // Gray Scale Level 52
-DATA(0x82);	 // Gray Scale Level 53
-DATA(0x87);	 // Gray Scale Level 54
-DATA(0x8C);	 // Gray Scale Level 55
-DATA(0x91);	 // Gray Scale Level 56
-DATA(0x96);	 // Gray Scale Level 57
-DATA(0x9B);	 // Gray Scale Level 58
-DATA(0xA0);	 // Gray Scale Level 59
-DATA(0xA5);	 // Gray Scale Level 60
-DATA(0xAA);	 // Gray Scale Level 61
-DATA(0xAF);	 // Gray Scale Level 62
-DATA(0xB4);	 // Gray Scale Level 63
-*/
-/*
+		DATA(0x10);	 // Gray Scale Level 15
+		DATA(0x11);	 // Gray Scale Level 16
+		DATA(0x12);	 // Gray Scale Level 17
+		DATA(0x13);	 // Gray Scale Level 18
+		DATA(0x15);	 // Gray Scale Level 19
+		DATA(0x17);	 // Gray Scale Level 20
+		DATA(0x19);	 // Gray Scale Level 21
+		DATA(0x1B);	 // Gray Scale Level 22
+		DATA(0x1D);	 // Gray Scale Level 23
+		DATA(0x1F);	 // Gray Scale Level 24
+		DATA(0x21);	 // Gray Scale Level 25
+		DATA(0x23);	 // Gray Scale Level 26
+		DATA(0x25);	 // Gray Scale Level 27
+		DATA(0x27);	 // Gray Scale Level 28
+		DATA(0x2A);	 // Gray Scale Level 29
+		DATA(0x2D);	 // Gray Scale Level 30
+		DATA(0x30);	 // Gray Scale Level 31
+		DATA(0x33);	 // Gray Scale Level 32
+		DATA(0x36);	 // Gray Scale Level 33
+		DATA(0x39);	 // Gray Scale Level 34
+		DATA(0x3C);	 // Gray Scale Level 35
+		DATA(0x3F);	 // Gray Scale Level 36
+		DATA(0x42);	 // Gray Scale Level 37
+		DATA(0x45);	 // Gray Scale Level 38
+		DATA(0x48);	 // Gray Scale Level 39
+		DATA(0x4C);	 // Gray Scale Level 40
+		DATA(0x50);	 // Gray Scale Level 41
+		DATA(0x54);	 // Gray Scale Level 42
+		DATA(0x58);	 // Gray Scale Level 43
+		DATA(0x5C);	 // Gray Scale Level 44
+		DATA(0x60);	 // Gray Scale Level 45
+		DATA(0x64);	 // Gray Scale Level 46
+		DATA(0x68);	 // Gray Scale Level 47
+		DATA(0x6C);	 // Gray Scale Level 48
+		DATA(0x70);	 // Gray Scale Level 49
+		DATA(0x74);	 // Gray Scale Level 50
+		DATA(0x78);	 // Gray Scale Level 51
+		DATA(0x7D);	 // Gray Scale Level 52
+		DATA(0x82);	 // Gray Scale Level 53
+		DATA(0x87);	 // Gray Scale Level 54
+		DATA(0x8C);	 // Gray Scale Level 55
+		DATA(0x91);	 // Gray Scale Level 56
+		DATA(0x96);	 // Gray Scale Level 57
+		DATA(0x9B);	 // Gray Scale Level 58
+		DATA(0xA0);	 // Gray Scale Level 59
+		DATA(0xA5);	 // Gray Scale Level 60
+		DATA(0xAA);	 // Gray Scale Level 61
+		DATA(0xAF);	 // Gray Scale Level 62
+		DATA(0xB4);	 // Gray Scale Level 63
+		*/
+	/*
 
-	DATA(0x00);
-	DATA(0x01);
-	DATA(0x02);
-	DATA(0x03);
-	DATA(0x04);
-	DATA(0x05);
-	DATA(0x06);
-	DATA(0x07);
-	DATA(0x08);
-	DATA(0x09);
-	DATA(0x0a);
-	DATA(0x0b);
-	DATA(0x0c);
-	DATA(0x0d);
-	DATA(0x0e);
-	DATA(0x0f);
-	DATA(0x10);
-	DATA(0x16);
-	DATA(0x18);
-	DATA(0x1a);
-	DATA(0x1b);
-	DATA(0x1C);
-	DATA(0x1D);
-	DATA(0x1F);
-	DATA(0x21);
-	DATA(0x23);
-	DATA(0x25);
-	DATA(0x27);
-	DATA(0x2A);
-	DATA(0x2D);
-	DATA(0x30);
-	DATA(0x33);
-	DATA(0x36);
-	DATA(0x39);
-	DATA(0x3C);
-	DATA(0x3F);
-	DATA(0x42);
-	DATA(0x45);
-	DATA(0x48);
-	DATA(0x4C);
-	DATA(0x50);
-	DATA(0x54);
-	DATA(0x58);
-	DATA(0x5C);
-	DATA(0x60);
-	DATA(0x64);
-	DATA(0x68);
-	DATA(0x6C);
-	DATA(0x70);
-	DATA(0x74);
-	DATA(0x78);
-	DATA(0x7D);
-	DATA(0x82);
-	DATA(0x87);
-	DATA(0x8C);
-	DATA(0x91);
-	DATA(0x96);
-	DATA(0x9B);
-	DATA(0xA0);
-	DATA(0xA5);
-	DATA(0xAA);
-	DATA(0xAF);
-	DATA(0xB4);
-*/
+	   DATA(0x00);
+	   DATA(0x01);
+	   DATA(0x02);
+	   DATA(0x03);
+	   DATA(0x04);
+	   DATA(0x05);
+	   DATA(0x06);
+	   DATA(0x07);
+	   DATA(0x08);
+	   DATA(0x09);
+	   DATA(0x0a);
+	   DATA(0x0b);
+	   DATA(0x0c);
+	   DATA(0x0d);
+	   DATA(0x0e);
+	   DATA(0x0f);
+	   DATA(0x10);
+	   DATA(0x16);
+	   DATA(0x18);
+	   DATA(0x1a);
+	   DATA(0x1b);
+	   DATA(0x1C);
+	   DATA(0x1D);
+	   DATA(0x1F);
+	   DATA(0x21);
+	   DATA(0x23);
+	   DATA(0x25);
+	   DATA(0x27);
+	   DATA(0x2A);
+	   DATA(0x2D);
+	   DATA(0x30);
+	   DATA(0x33);
+	   DATA(0x36);
+	   DATA(0x39);
+	   DATA(0x3C);
+	   DATA(0x3F);
+	   DATA(0x42);
+	   DATA(0x45);
+	   DATA(0x48);
+	   DATA(0x4C);
+	   DATA(0x50);
+	   DATA(0x54);
+	   DATA(0x58);
+	   DATA(0x5C);
+	   DATA(0x60);
+	   DATA(0x64);
+	   DATA(0x68);
+	   DATA(0x6C);
+	   DATA(0x70);
+	   DATA(0x74);
+	   DATA(0x78);
+	   DATA(0x7D);
+	   DATA(0x82);
+	   DATA(0x87);
+	   DATA(0x8C);
+	   DATA(0x91);
+	   DATA(0x96);
+	   DATA(0x9B);
+	   DATA(0xA0);
+	   DATA(0xA5);
+	   DATA(0xAA);
+	   DATA(0xAF);
+	   DATA(0xB4);
+	   */
 	// Clear screen
 	lcdFillRGB(0,0,0);
 
@@ -341,31 +400,13 @@ void lcdFillRGB(uint8_t r,uint8_t g,uint8_t b)
 	ssd1351SetCursor(0, 0);
 
 #ifdef C262K
-
-	
-
-	uint16_t port_r = port2_table[r>>2];
-	uint16_t port_g = port2_table[g>>2];
-	uint16_t port_b = port2_table[b>>2];
-
-
-	SET_DC;	
 	for (x=1; x<=ssd1351Properties.width ;x++)
 	{
 		for (y=1; y<= ssd1351Properties.height;y++)
 		{
-			SET_E;
-			LPC_GPIO2->DATA = port_r;
-			__ASM volatile ("nop");
-			CLR_E;
-			SET_E;
-			LPC_GPIO2->DATA = port_g;
-			__ASM volatile ("nop");
-			CLR_E;
-			SET_E;
-			LPC_GPIO2->DATA = port_b;
-			__ASM volatile ("nop");
-			CLR_E;
+			DATA(r);
+			DATA(g);
+			DATA(b);
 		}
 	}
 #endif
--- drivers/ssd1351.h
+++ drivers/ssd1351.h
@@ -6,8 +6,8 @@
 #define LED_WIDTH 128
 #define LED_HEIGHT 128
 
-//#define I6800
-//#define C262K
+#define I6800
+#define C262K
 
 
 
@@ -19,8 +19,8 @@
 
 typedef struct
 {
-  uint16_t width;         // LCD width in pixels (default orientation)
-  uint16_t height;        // LCD height in pixels (default orientation)
+	uint16_t width;         // LCD width in pixels (default orientation)
+	uint16_t height;        // LCD height in pixels (default orientation)
 } lcdProperties_t;
 
 
@@ -69,8 +69,8 @@ typedef struct
 #define CLR_D2 (LPC_GPIO->B1[22] = 0)
 #define SET_D3 (LPC_GPIO->B0[15] = 1)
 #define CLR_D3 (LPC_GPIO->B0[15] = 0)
-#define SET_D4 (LPC_GPIO->B0[16] = 1)
-#define CLR_D4 (LPC_GPIO->B0[16] = 0)
+#define SET_D4 (LPC_GPIO->B1[20] = 1)
+#define CLR_D4 (LPC_GPIO->B1[20] = 0)
 #define SET_D5 (LPC_GPIO->B0[17] = 1)
 #define CLR_D5 (LPC_GPIO->B0[17] = 0)
 #define SET_D6 (LPC_GPIO->B1[16] = 1)
@@ -89,45 +89,45 @@ typedef struct
 // SSD1351 Commands
 enum
 {
-  SSD1351_CMD_SETCOLUMNADDRESS          = 0x15,
-  SSD1351_CMD_SETROWADDRESS             = 0x75,
-  SSD1351_CMD_WRITERAM                  = 0x5C, // Write data to GRAM and increment until another command is sent
-  SSD1351_CMD_READRAM                   = 0x5D, // Read data from GRAM and increment until another command is sent
-  SSD1351_CMD_COLORDEPTH                = 0xA0, // Numerous functions include increment direction ... see DS 
-                                                // A0[0] = Address Increment Mode (0 = horizontal, 1 = vertical)
-                                                // A0[1] = Column Address Remap (0 = left to right, 1 = right to left)
-                                                // A0[2] = Color Remap (0 = ABC, 1 = CBA) - HW RGB/BGR switch
-                                                // A0[4] = COM Scan Direction (0 = top to bottom, 1 = bottom to top)
-                                                // A0[5] = Odd/Even Paid Split
-                                                // A0[7:6] = Display Color Mode (00 = 8-bit, 01 = 65K, 10/11 = 262K, 8/16-bit interface only)
-  SSD1351_CMD_SETDISPLAYSTARTLINE       = 0xA1,
-  SSD1351_CMD_SETDISPLAYOFFSET          = 0xA2, 
-  SSD1351_CMD_SETDISPLAYMODE_ALLOFF     = 0xA4, // Force entire display area to grayscale GS0
-  SSD1351_CMD_SETDISPLAYMODE_ALLON      = 0xA5, // Force entire display area to grayscale GS63
-  SSD1351_CMD_SETDISPLAYMODE_RESET      = 0xA6, // Resets the display area relative to the above two commands
-  SSD1351_CMD_SETDISPLAYMODE_INVERT     = 0xA7, // Inverts the display contents (GS0 -> GS63, GS63 -> GS0, etc.)
-  SSD1351_CMD_FUNCTIONSELECTION         = 0xAB, // Enable/Disable the internal VDD regulator
-  SSD1351_CMD_SLEEPMODE_DISPLAYOFF      = 0xAE, // Sleep mode on (display off)
-  SSD1351_CMD_SLEEPMODE_DISPLAYON       = 0xAF, // Sleep mode off (display on)
-  SSD1351_CMD_SETPHASELENGTH            = 0xB1, // Larger capacitance may require larger delay to discharge previous pixel state
-  SSD1351_CMD_ENHANCEDDRIVINGSCHEME     = 0xB2, 
-  SSD1351_CMD_SETFRONTCLOCKDIV          = 0xB3, // DCLK divide ration fro CLK (from 1 to 16)
-  SSD1351_CMD_SETSEGMENTLOWVOLTAGE      = 0xB4,
-  SSD1351_CMD_SETGPIO                   = 0xB5,
-  SSD1351_CMD_SETSECONDPRECHARGEPERIOD  = 0xB6,
-  SSD1351_CMD_GRAYSCALELOOKUP           = 0xB8,
-  SSD1351_CMD_LINEARLUT                 = 0xB9,
-  SSD1351_CMD_SETPRECHARGEVOLTAGE       = 0xBB,
-  SSD1351_CMD_SETVCOMHVOLTAGE           = 0xBE,
-  SSD1351_CMD_SETCONTRAST               = 0xC1,
-  SSD1351_CMD_MASTERCONTRAST            = 0xC7,
-  SSD1351_CMD_SETMUXRRATIO              = 0xCA,
-  SSD1351_CMD_NOP3                      = 0xD1,
-  SSD1351_CMD_NOP4                      = 0xE3,
-  SSD1351_CMD_SETCOMMANDLOCK            = 0xFD,
-  SSD1351_CMD_HORIZONTALSCROLL          = 0x96,
-  SSD1351_CMD_STOPMOVING                = 0x9E,
-  SSD1351_CMD_STARTMOVING               = 0x9F  
+	SSD1351_CMD_SETCOLUMNADDRESS          = 0x15,
+	SSD1351_CMD_SETROWADDRESS             = 0x75,
+	SSD1351_CMD_WRITERAM                  = 0x5C, // Write data to GRAM and increment until another command is sent
+	SSD1351_CMD_READRAM                   = 0x5D, // Read data from GRAM and increment until another command is sent
+	SSD1351_CMD_COLORDEPTH                = 0xA0, // Numerous functions include increment direction ... see DS 
+// A0[0] = Address Increment Mode (0 = horizontal, 1 = vertical)
+// A0[1] = Column Address Remap (0 = left to right, 1 = right to left)
+// A0[2] = Color Remap (0 = ABC, 1 = CBA) - HW RGB/BGR switch
+// A0[4] = COM Scan Direction (0 = top to bottom, 1 = bottom to top)
+// A0[5] = Odd/Even Paid Split
+// A0[7:6] = Display Color Mode (00 = 8-bit, 01 = 65K, 10/11 = 262K, 8/16-bit interface only)
+	SSD1351_CMD_SETDISPLAYSTARTLINE       = 0xA1,
+	SSD1351_CMD_SETDISPLAYOFFSET          = 0xA2, 
+	SSD1351_CMD_SETDISPLAYMODE_ALLOFF     = 0xA4, // Force entire display area to grayscale GS0
+	SSD1351_CMD_SETDISPLAYMODE_ALLON      = 0xA5, // Force entire display area to grayscale GS63
+	SSD1351_CMD_SETDISPLAYMODE_RESET      = 0xA6, // Resets the display area relative to the above two commands
+	SSD1351_CMD_SETDISPLAYMODE_INVERT     = 0xA7, // Inverts the display contents (GS0 -> GS63, GS63 -> GS0, etc.)
+	SSD1351_CMD_FUNCTIONSELECTION         = 0xAB, // Enable/Disable the internal VDD regulator
+	SSD1351_CMD_SLEEPMODE_DISPLAYOFF      = 0xAE, // Sleep mode on (display off)
+	SSD1351_CMD_SLEEPMODE_DISPLAYON       = 0xAF, // Sleep mode off (display on)
+	SSD1351_CMD_SETPHASELENGTH            = 0xB1, // Larger capacitance may require larger delay to discharge previous pixel state
+	SSD1351_CMD_ENHANCEDDRIVINGSCHEME     = 0xB2, 
+	SSD1351_CMD_SETFRONTCLOCKDIV          = 0xB3, // DCLK divide ration fro CLK (from 1 to 16)
+	SSD1351_CMD_SETSEGMENTLOWVOLTAGE      = 0xB4,
+	SSD1351_CMD_SETGPIO                   = 0xB5,
+	SSD1351_CMD_SETSECONDPRECHARGEPERIOD  = 0xB6,
+	SSD1351_CMD_GRAYSCALELOOKUP           = 0xB8,
+	SSD1351_CMD_LINEARLUT                 = 0xB9,
+	SSD1351_CMD_SETPRECHARGEVOLTAGE       = 0xBB,
+	SSD1351_CMD_SETVCOMHVOLTAGE           = 0xBE,
+	SSD1351_CMD_SETCONTRAST               = 0xC1,
+	SSD1351_CMD_MASTERCONTRAST            = 0xC7,
+	SSD1351_CMD_SETMUXRRATIO              = 0xCA,
+	SSD1351_CMD_NOP3                      = 0xD1,
+	SSD1351_CMD_NOP4                      = 0xE3,
+	SSD1351_CMD_SETCOMMANDLOCK            = 0xFD,
+	SSD1351_CMD_HORIZONTALSCROLL          = 0x96,
+	SSD1351_CMD_STOPMOVING                = 0x9E,
+	SSD1351_CMD_STARTMOVING               = 0x9F  
 };
 
 void lcdInit(void);
